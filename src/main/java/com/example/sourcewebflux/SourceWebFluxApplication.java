f1926fd829c23eb185dc31a1ff8405d771516808
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-11-27 13:20:03 +0500

    Initial commit

---
+++ src/main/java/com/example/sourcewebflux/SourceWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.sourcewebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceWebFluxApplication.class, args);
+    }
+
+}
